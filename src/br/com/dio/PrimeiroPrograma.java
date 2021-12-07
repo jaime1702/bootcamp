ca13a8629f54d0f8b34a93ff38ab6f60aeb955f9
Jaime Furlan <jaimeluis17furlan@gmail.com>
2021-12-07 20:04:10 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+	}
+		/*int a =10;
+		int b =10;
+		System.out.println("fala gabi or "+ a);
+	}*/
+
+}
